402f6f81bb23e09b86e652acdf6e2deb980a84c4
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-03-05 19:26:34 +0100

    faces

--- frontng/src/app/app.module.ts
+++ frontng/src/app/app.module.ts
@@ -1,22 +1,22 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CreateprodComponent } from './createprod/createprod.component';
 import { ReadprodComponent } from './readprod/readprod.component';
-import { ProductsComponent } from './classes/products/products.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CreateprodComponent,
     ReadprodComponent,
-    ProductsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontng/src/app/classes/products.ts
@@ -0,0 +1,7 @@
+export class Products {
+    id:number;
+    title:string;
+    productDescription:string;
+    productBrand:string;
+    price:number;
+}
