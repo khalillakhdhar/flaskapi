807303946784881405ca9de3158a19de54484591
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-04-02 18:56:30 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,43 +4,13 @@ from flask_sqlalchemy import SQLAlchemy
 from marshmallow_sqlalchemy import ModelSchema
 from marshmallow import fields
 from flask_cors import CORS, cross_origin
-
+from model.products import Product,ProductSchema
 app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost:3306/demoflask'
 db = SQLAlchemy(app)
 
-###Models####
-class Product(db.Model):
-    __tablename__ = "products"
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(20))
-    productDescription = db.Column(db.String(100))
-    productBrand = db.Column(db.String(20))
-    price = db.Column(db.Integer)
-
-    def create(self):
-      db.session.add(self)
-      db.session.commit()
-      return self
-    def __init__(self,title,productDescription,productBrand,price):
-        self.title = title
-        self.productDescription = productDescription
-        self.productBrand = productBrand
-        self.price = price
-    def __repr__(self):
-        return '' % self.id
-db.create_all()
-class ProductSchema(ModelSchema):
-    class Meta(ModelSchema.Meta):
-        model = Product
-        sqla_session = db.session
-    id = fields.Number(dump_only=True)
-    title = fields.String(required=True)
-    productDescription = fields.String(required=True)
-    productBrand = fields.String(required=True)
-    price = fields.Number(required=True)
 
 @app.route('/products', methods = ['GET'])
 @cross_origin()
